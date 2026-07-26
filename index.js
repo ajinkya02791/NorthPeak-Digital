e37b89717c00f4cfc7467b577f7c2dbaca2c90a9
ajinkya <77152422+ajinkya02791@users.noreply.github.com>
2026-07-26 12:10:27 +0000

    use inbuilt validation checking and alert message on successful valid inputs

--- index.js
+++ index.js
@@ -9,3 +9,40 @@ btn.addEventListener("click",  () => {
     const expanded = navLinks.classList.contains("open")
     btn.setAttribute("aria-expanded" , expanded);
 })
+
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("contact-us-message");
+
+
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const messageError = document.getElementById("messageError");
+
+
+const form = document.getElementById("form");
+
+function validateField(input, errorElement) {
+    if (input.checkValidity()) {
+        return true;
+    }
+
+    return false;
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const validName = validateField(name, nameError);
+    const validEmail= validateField(email, emailError);
+    const validMessage = validateField(message, messageError);
+
+
+    
+    if(validEmail && validEmail && validMessage) {
+        alert("Thank you! Your message has been sent successfully.");
+        form.reset()
+    }
+
+})
